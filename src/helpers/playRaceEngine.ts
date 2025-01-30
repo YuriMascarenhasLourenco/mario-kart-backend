97fda8ee1b149a60065f115a1019c828565cb2dd
Yuri Lourenço <lourenco99yuri@gmail.com>
2025-01-30 16:03:35 -0300

    raceEngine running

--- src/helpers/playRaceEngine.ts
+++ src/helpers/playRaceEngine.ts
@@ -24,9 +24,17 @@ const logs = async (
   skill: number,
 ) => {
   console.log(
-    `${player.nome} rolou um dado 🎲 de ${block} ${diceresult} +${skill} `,
+    `${player.nome} rolou um dado 🎲 de ${block} ${diceresult} +${skill} = ${diceresult + skill}`,
   );
 };
+const winnerGame=(player1:Racer,player2:Racer)=>{
+if(player1.pontos>player2.pontos){
+  console.log(`${player1.nome} venceu a corrida!`)
+}
+if(player2.pontos>player1.pontos){
+  console.log(`${player2.nome} venceu a corrida!`)
+}
+}
 export const playRaceEngine = async (player1: Racer, player2: Racer) => {
   for (let i = 1; i <= 5; i++) {
     console.log(`🏁 Rodada ${i}`);
@@ -62,10 +70,28 @@ export const playRaceEngine = async (player1: Racer, player2: Racer) => {
       );
     }
     if (block === 'CONFRONTO') {
-      totalSkil1 = diceresult1 + player1.poder;
-      totalSkil2 = diceresult2 + player2.poder;
+      let power1 = diceresult1 + player1.poder;
+      let power2 = diceresult2 + player2.poder;
+
+      console.log(`${player1.nome} confrontou com ${player2.nome}`);
       await logs('CONFRONTO', player1, diceresult1, player1.poder);
       await logs('CONFRONTO', player2, diceresult2, player2.poder);
+      player2.pontos -= power1 > power2 && player2.pontos > 0 ? 1 : 0;
+      player1.pontos -= power2 > power1 && player1.pontos > 0 ? 1 : 0;
+      console.log(
+        power1 === power2
+          ? 'EMPATE! O confronto tá acirrado, ninguém perdeu ponto'
+          : '',
+      );
+    }
+
+    if (totalSkil1 > totalSkil2) {
+      console.log(`no bloco ${block} ${player1.nome} fez um ponto!`);
+      player1.pontos++;
+    } else if (totalSkil2 > totalSkil1) {
+      console.log(`noo bloco ${block} ${player2.nome} fez um ponto!`);
+      player2.pontos++;
     }
   }
+  winnerGame(player1,player2);
 };
